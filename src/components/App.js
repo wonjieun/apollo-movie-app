6f767aab35248a1995ee483f3f28ae81c48a68ec
Jina <wonjieunkk@gmail.com>
2022-05-31 14:25:00 +0900

    👌 IMPROVE: update react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from '../routes/Home';
 import Detail from '../routes/Detail';
 
 function App() {
   return (
-    <Router>
+    <Routes>
       <Route exact path="/" component={Home} />
       <Route path="/:id" component={Detail} />
-    </Router>
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
 import client from './apollo';
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>
 );
